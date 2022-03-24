6983bdc4f16a55bbd8e35798f0415b33a50b8d1f
William05Cardozo <3784@holbertonschool.com>
2022-03-24 12:42:39 -0700

    5-Binary-Tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,13 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node->left && node->right)
+	{
+		return (1);
+	}
+	else
 	{
 		return (0);
 	}
-	return (node->left && node->right);
+	return (0);
 }
