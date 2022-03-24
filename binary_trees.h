9f8bd4b30952ca081a264e17898b3ccc92a32c3f
William05Cardozo <3784@holbertonschool.com>
2022-03-23 18:31:56 -0700

    Binary_trees.h correction

--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ typedef struct binary_tree_s heap_t;
 
 /*Functions Prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
@@ -48,5 +48,4 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /*Functions of binary_tree_print.h*/
 void binary_tree_print(const binary_tree_t *tree);
-
 #endif
