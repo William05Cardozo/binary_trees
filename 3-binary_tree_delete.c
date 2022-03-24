76ec19e32aba75ca27f07f331fd69d0d47111767
William05Cardozo <3784@holbertonschool.com>
2022-03-23 21:46:46 -0700

    3-Binary-Tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * _deleteTree - Auxiliary function 
+ * @tree: Pointer to the tree
+ * Return: Null
+ **/
+void _deleteTree(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+	_deleteTree(tree->left);
+	_deleteTree(tree->right);
+	free(tree);
+}
+
+/**
+ * binary_tree_delete - Delete tree
+ * @tree: Pointer to the tree
+ * Return: No line 
+ **/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	_deleteTree(tree);
+	tree = NULL;
+}
