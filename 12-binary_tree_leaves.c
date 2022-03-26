3576a76425d7995dbc47aaf33ce43cb904435d30
William05Cardozo <3784@holbertonschool.com>
2022-03-25 20:52:35 -0700

    12-Binary_tree

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Function we use for calculate number of leaves
+ * @tree: Pointer to parent node
+ * nleft: Varaible
+ * nright: Variable
+ * Return: Number ofleaves
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t nleft = 0, nright = 0;
+
+	if(tree == NULL)
+		return (0);
+	if(tree->left == NULL && tree->right == NULL)
+		return (1);
+	nleft = binary_tree_leaves(tree->left);
+	nright = binary_tree_leaves(tree->right);
+
+	return (nleft + nright);
+}
