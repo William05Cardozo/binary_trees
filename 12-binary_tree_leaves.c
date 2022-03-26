c66af53d6e1762a48ea8c50ccd03264bdb7faaf6
William05Cardozo <3784@holbertonschool.com>
2022-03-25 20:57:07 -0700

    12-Binary_tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,9 +12,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t nleft = 0, nright = 0;
 
-	if(tree == NULL)
+	if (tree == NULL)
 		return (0);
-	if(tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	nleft = binary_tree_leaves(tree->left);
 	nright = binary_tree_leaves(tree->right);
