42aa6640ec2b5544ff2d753c7d1651ef82ffdd0f
William05Cardozo <3784@holbertonschool.com>
2022-03-25 20:33:07 -0700

    11-Binary_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,14 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t nsize;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+	nsize = binary_tree_size(tree->left) + 1;
+	nsize += binary_tree_size(tree->right);
+
+	return (nsize);
 }
